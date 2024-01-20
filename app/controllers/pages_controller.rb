32a2983a142d91220a2e767470506d8c7605663a
richie1988 <richiessikaonga@gmail.com>
2024-01-20 21:17:18 +0200

    Bugs handling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:splash]
   def splash
     if user_signed_in?
-      redirect_to user_expenses_path(current_user)
+      redirect_to user_categories_path(current_user)
     else
       render :splash
     end
