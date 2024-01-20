47ebb09f361f7169b014b800e2c58d893cbeb118
richie1988 <richiessikaonga@gmail.com>
2024-01-20 19:09:22 +0200

    Profile updates and implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in(@user)
-      redirect_to @user, notice: 'Successfully signed up.' # Redirect to @user instead of root_path
+      redirect_to @user, notice: 'Successfully signed up.'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -21,10 +21,12 @@ class UsersController < ApplicationController
   def edit; end
 
   def update
+    @user = User.find(params[:id])
+
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.' # Redirect to @user after successful update
+      redirect_to @user, notice: 'User was successfully updated.'
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -40,6 +42,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :name)
+    params.require(:user).permit(:email, :password, :name, :image)
   end
 end
